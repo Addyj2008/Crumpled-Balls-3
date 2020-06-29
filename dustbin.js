7684df2bd64d9f82f424fe410d56e169ccbf10a0
Addyj2008 <66421840+Addyj2008@users.noreply.github.com>
2020-06-29 20:20:44 +0530

    Update dustbin.js

--- dustbin.js
+++ dustbin.js
@@ -3,7 +3,7 @@ class Dustbin {
     constructor (x, y, width, height) {
         this.width = width;
         this.height = height;
-        this.image = loadImage('Images/dustbingreen.png');
+        this.image = loadImage('dustbingreen.png');
         this.bottomBody = Bodies.rectangle(x, y + this.height/2 - 10, this.width, 20, {isStatic:true});
         Worlds.add(world, this.bottomBody);
         this.rightBody = Bodies.rectangle(x + this.width/2 - 10, y - 10, 20, this.height - 20, {isStatic:true});
